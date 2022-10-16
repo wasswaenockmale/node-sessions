9838c91e3213f1166ec3164bbef2b742b08cb9ec
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-16 23:21:07 +0300

    Different ways of exporting modules in nodeJS

--- session5/tutorial.js
+++ session5/tutorial.js
@@ -10,6 +10,8 @@ class SomeMathObject{
     }
 }
 
-module.exports.sum = sum
-module.exports.PI = PI
-module.exports.SomeMathObject = SomeMathObject
+// module.exports.sum = sum
+// module.exports.PI = PI
+// module.exports.SomeMathObject = SomeMathObject
+
+module.exports = {sum: sum, PI : PI, SomeMathObject: SomeMathObject}
