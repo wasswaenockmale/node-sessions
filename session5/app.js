f8a313cb03ca4a0a5476145a93ef31373bce970b
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-16 15:14:23 +0300

    I have called the other values

---
+++ session5/app.js
@@ -0,0 +1,4 @@
+const tutorial = require("./tutorial");
+
+console.log(tutorial.sum(12,3))
+const some = new tutorial.SomeMathObject
---
+++ session5/tutorial.js
@@ -0,0 +1,15 @@
+const sum = (a,b)=>{
+    return a+b
+}
+
+const PI = 3.14;
+
+class SomeMathObject{
+    constructor(){
+        console.log("This is a class")
+    }
+}
+
+module.exports.sum = sum
+module.exports.PI = PI
+module.exports.SomeMathObject = SomeMathObject
