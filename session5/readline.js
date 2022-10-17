ad90ef3f15255086504593480ff4a32ecd4abb36
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-18 01:45:13 +0300

    I am learning nodeJS

--- session5/readline.js
+++ session5/readline.js
@@ -9,8 +9,19 @@ const num2 = Math.floor((Math.random() * 10) + 1)
 const ans = num1 + num2
 // now create the question 
 r1.question(`What is the ${num1} + ${num2}\n`,(userInput)=>{
-    if(userInput == ans){
+    if(userInput.trim() == ans){
         r1.close()
+    }else{
+        r1.setPrompt("Wrong answer\n")
+        r1.prompt()
+        r1.on("line",()=>{
+            if(userInput.trim() == ans){
+                r1.close()
+            }else{
+                r1.setPrompt("Your answer is wrong\n")
+                r1.prompt()
+            }
+        })
     }
 })
 
