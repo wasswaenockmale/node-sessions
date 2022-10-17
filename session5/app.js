71cc2a8e9740e281661dc7c8337a5af4a16c6c40
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-18 01:01:51 +0300

    I have practiced the dynamic method naming in javascript

--- session5/app.js
+++ session5/app.js
@@ -1,17 +1,20 @@
-// const tutorial = require("./tutorial");
+const tutorial = require("./tutorial");
 
 // console.log(tutorial.sum(12,3))
-// // const some = new tutorial.SomeMathObject
+const some = new tutorial.SomeMathObject
+console.log(some.add(1,2))
+console.log(some[3]())
 // new tutorial.SomeMathObject()
 
 
 // What does the EventEmitter do in node 
-const EventEmitter = require("events");
+// const EventEmitter = require("events");
 
-const eventEmitter = new EventEmitter()
+// const eventEmitter = new EventEmitter()
 
-eventEmitter.on("tutorial", ()=>{
-    console.log("Tutorial event has occured");
-});
+// eventEmitter.on("tutorial", ()=>{
+//     console.log("Tutorial event has occured");
+// });
 
-eventEmitter.emit("tutorial");
+// eventEmitter.emit("tutorial");
+
