388fb839c67bf487ac22c9a00205e98dae10c46c
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-18 00:45:15 +0300

    I have events emitter in javascript nodeJS

--- session5/app.js
+++ session5/app.js
@@ -1,5 +1,17 @@
-const tutorial = require("./tutorial");
+// const tutorial = require("./tutorial");
 
-console.log(tutorial.sum(12,3))
-// const some = new tutorial.SomeMathObject
-new tutorial.SomeMathObject()
+// console.log(tutorial.sum(12,3))
+// // const some = new tutorial.SomeMathObject
+// new tutorial.SomeMathObject()
+
+
+// What does the EventEmitter do in node 
+const EventEmitter = require("events");
+
+const eventEmitter = new EventEmitter()
+
+eventEmitter.on("tutorial", ()=>{
+    console.log("Tutorial event has occured");
+});
+
+eventEmitter.emit("tutorial");
