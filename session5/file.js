45cbe1cc26f2b552b3aeae91f2b22e4b7be61849
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-18 11:56:17 +0300

    Deleted all files that are in more

--- session5/file.js
+++ session5/file.js
@@ -16,34 +16,34 @@ const fs = require("fs")
 // });
 
 // Create the directory
-fs.mkdir("./tutorial/example",(err)=>{
-    if(err)
-        console.log(err)
-    else{
-        // If the directory is created successfully, then create a file in it
-        fs.writeFile("./tutorial/example/hello.js","console.log(\"Hello, world\")",(err1)=>{
-            if(err)
-                console.log(err1)
-            else{
-                // If the file is created successfully, then read the file.
-                fs.readFile("./tutorial/example/hello.js","utf-8",(err2,file)=>{
-                    if(err2)
-                        console.log(err2)
-                    else{
-                        console.log(file)
-                    }
-                });
-            }
-        });
-    }
-});
+// fs.mkdir("./tutorial/example",(err)=>{
+//     if(err)
+//         console.log(err)
+//     else{
+//         // If the directory is created successfully, then create a file in it
+//         fs.writeFile("./tutorial/example/hello.js","console.log(\"Hello, world\")",(err1)=>{
+//             if(err)
+//                 console.log(err1)
+//             else{
+//                 // If the file is created successfully, then read the file.
+//                 fs.readFile("./tutorial/example/hello.js","utf-8",(err2,file)=>{
+//                     if(err2)
+//                         console.log(err2)
+//                     else{
+//                         console.log(file)
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// });
 
 //Now, use the fs file to read the files in the more folder and delete them.
-fs.readdir("./tutorial/more",(err,files)=>{
+fs.readdir("./tutorial/more/",(err,files)=>{
     if(err)
         console.log(err)
     else{
-        for(const file in files){
+        for(const file of files){
             fs.unlink("./tutorial/more/"+file, (err1)=>{
                 if(err1)
                     console.log(err1)
