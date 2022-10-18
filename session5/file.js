0ccfd2d896b1715590c629f53fbb66e5017a1507
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-18 11:43:57 +0300

    Practicing the fs module in node

--- session5/file.js
+++ session5/file.js
@@ -1,9 +1,39 @@
 const fs = require("fs")
 
-fs.writeFile("names.txt","This is the file",(err)=>{
-    if(err){
+// fs.writeFile("names.txt","This is the file",(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("file created successfully")
+//     }
+// });
+
+// fs.unlink("names.txt",(err)=>{
+//     if(err)
+//         console.log(err)
+//     else
+//         console.log("Successfully deleted the file")
+// });
+
+// Create the directory
+fs.mkdir("tutorial",(err)=>{
+    if(err)
         console.log(err)
-    }else{
-        console.log("file created successfully")
+    else{
+        // If the directory is created successfully, then create a file in it
+        fs.writeFile("./tutorial/example.js","console.log(\"Hello, world\")",(err1)=>{
+            if(err)
+                console.log(err1)
+            else{
+                // If the file is created successfully, then read the file.
+                fs.readFile("./tutorial/example.js","utf-8",(err2,file)=>{
+                    if(err2)
+                        console.log(err2)
+                    else{
+                        console.log(file)
+                    }
+                });
+            }
+        });
     }
 });
